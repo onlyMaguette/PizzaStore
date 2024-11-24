2387dbaa1faf7577c99cb487f0eb65e49041487f
Maguette NDIAYE <maguettendiaye1@esp.sn>
2024-11-24 14:18:57 +0000

    Configuration du projet

---
+++ Pizza.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace PizzaStore.Models
+{
+    public class PizzaEhod
+    {
+        public int IdEhod { get; set; }
+        public string? NomEhod { get; set; }
+        public string? DescriptionEhod { get; set; }
+    }
+}
